0c14ea934f599c73129ff5363d380ba2d4554d65
markriis <markriis@ut.ee>
2024-11-17 15:02:04 +0200

    Likes reset to 0 instead of their initial value read task 4.3 wrong, now it's fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,101 +1,97 @@
 import { createStore } from 'vuex';
 
-let initialPosts = [
-  {
-    // need postids to distinguish them when mutating via vue store
-    // passing index as key automatically in the MainPage component, although
-    // this wont suffice and might lead to problems when making an actual application
-    // getting postIds from a database would be a more proper way to do it (or hardcode them in the data file here but thats more work)
-    author: "User1",
-    createTime: "2022-10-22",
-    content: "black cat gif",
-    imageURL: "https://wxcoy.cc/fail/00dc9de6-0c22-4241-bbe9-019df2b26fb8",
-    profileIcon: require('@/assets/profile-icon.png'), 
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'), 
-    likes: 6,
-  },
-  {
-    author: "User2",
-    createTime: "2022-10-22",
-    content: "Tartu 2022",
-    imageURL: "https://media.tenor.com/WwBJIu1sPMoAAAAM/cat-blink.gif",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'), 
-    likes: 54,
-  },
-  {
-    author: "User3",
-    createTime: "2022-10-21",
-    content: "More cats please",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 10,
-  },
-  {
-    author: "User4",
-    createTime: "2022-10-2",
-    content: "Anyone knows in which room is the lab today?",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 7,
-  },
-  {
-    author: "User5",
-    createTime: "2022-09-23",
-    content: "sügis algas :(",
-    imageURL: require('@/assets/no-no-no-no-cat.gif'),
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 0,
-  }, 
-  {
-    author: "User6",
-    createTime: "2022-10-30",
-    content: "Anyone else excited for the new season?",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 5,
-  }, 
-  {
-    author: "User7",
-    createTime: "2022-10-1",
-    content: "Can we get more dog photos too?",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 1,
-  }, 
-  {
-    author: "User8",
-    createTime: "2022-10-1",
-    content: "cool",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 2,
-  },
-  {
-    author: "User9",
-    createTime: "2022-10-1",
-    content: "working late...",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 2,
-  },
-  {
-    author: "User10",
-    createTime: "2022-10-1",
-    content: "Where is the best spot to study on campus?",
-    profileIcon: require('@/assets/profile-icon.png'),
-    likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
-    likes: 0,
-  }
-];
-
-initialPosts.forEach( p => p.initialLikes = p.likes );
-
 export default createStore({
   strict: true,
   state: {
-    posts: initialPosts,
+    posts: [
+      {
+        // need postids to distinguish them when mutating via vue store
+        // passing index as key automatically in the MainPage component, although
+        // this wont suffice and might lead to problems when making an actual application
+        // getting postIds from a database would be a more proper way to do it (or hardcode them in the data file here but thats more work)
+        author: "User1",
+        createTime: "2022-10-22",
+        content: "black cat gif",
+        imageURL: "https://wxcoy.cc/fail/00dc9de6-0c22-4241-bbe9-019df2b26fb8",
+        profileIcon: require('@/assets/profile-icon.png'), 
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'), 
+        likes: 6,
+      },
+      {
+        author: "User2",
+        createTime: "2022-10-22",
+        content: "Tartu 2022",
+        imageURL: "https://media.tenor.com/WwBJIu1sPMoAAAAM/cat-blink.gif",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'), 
+        likes: 54,
+      },
+      {
+        author: "User3",
+        createTime: "2022-10-21",
+        content: "More cats please",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 10,
+      },
+      {
+        author: "User4",
+        createTime: "2022-10-2",
+        content: "Anyone knows in which room is the lab today?",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 7,
+      },
+      {
+        author: "User5",
+        createTime: "2022-09-23",
+        content: "sügis algas :(",
+        imageURL: require('@/assets/no-no-no-no-cat.gif'),
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 0,
+      }, 
+      {
+        author: "User6",
+        createTime: "2022-10-30",
+        content: "Anyone else excited for the new season?",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 5,
+      }, 
+      {
+        author: "User7",
+        createTime: "2022-10-1",
+        content: "Can we get more dog photos too?",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 1,
+      }, 
+      {
+        author: "User8",
+        createTime: "2022-10-1",
+        content: "cool",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 2,
+      },
+      {
+        author: "User9",
+        createTime: "2022-10-1",
+        content: "working late...",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 2,
+      },
+      {
+        author: "User10",
+        createTime: "2022-10-1",
+        content: "Where is the best spot to study on campus?",
+        profileIcon: require('@/assets/profile-icon.png'),
+        likeIcon: require('@/assets/Icon-like-button-transparent-PNG.png'),
+        likes: 0,
+      }
+    ],
   },
   getters: {
     allPosts: (state) => state.posts,
@@ -124,7 +120,7 @@ export default createStore({
 
     // clientside reset
     resetLikes( state ) {
-      state.posts.forEach( ( p, idx ) => p.likes = initialPosts.at( idx ).initialLikes );
+      state.posts.forEach( ( p ) => p.likes = 0 );
     }
   },
   actions: {
